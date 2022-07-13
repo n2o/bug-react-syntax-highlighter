82ad58e29d0f03f480b87a40bf5b08f5510b6dfd
Christian Meter <cmeter@googlemail.com>
2022-07-13 22:27:42 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+const defaultTheme = require('./public/node_modules/tailwindcss/defaultTheme')
+
+module.exports = {
+  content: ["./public/js/compiled/cljs-runtime/playground.*.js",
+            "./public/js/compiled/main.js"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+}
