53c67ad6de3d855acffcd5cf5e8255f127e626fb
Christian Meter <cmeter@googlemail.com>
2022-07-15 10:07:35 +0200

    Use light version of react-syntax-highlighter

--- src/main/playground/core.cljs
+++ src/main/playground/core.cljs
@@ -1,17 +1,16 @@
 (ns playground.core
-  (:require ["highlight.js/lib/core" :as hljs]
-            ["highlight.js/lib/languages/clojure" :as hl-clojure]
-            ["react-syntax-highlighter" :refer [Prism]]
-            ["react-syntax-highlighter/dist/esm/styles/prism" :refer [github]]
+  (:require ["react-syntax-highlighter" :rename {Light SyntaxHighlighter}]
+            ["react-syntax-highlighter/dist/esm/languages/hljs/clojure$default" :as clojure-js]
+            ["react-syntax-highlighter/dist/esm/styles/hljs" :refer [github]]
             [goog.dom :as gdom]
             [reagent.dom]))
 
-(hljs/registerLanguage "clojure" hl-clojure)
+(.registerLanguage SyntaxHighlighter "clojure", clojure-js)
 
 (defn- main []
   [:main.container.mx-auto.pt-5
    [:h1 "Welcome to your app"]
-   [:> Prism {:language "clojure" :style github}
+   [:> SyntaxHighlighter {:language "clojure" :style github}
     "(welcome! :you)"]])
 
 ;; -----------------------------------------------------------------------------
